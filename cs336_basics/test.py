304b10f2c3a858cb9f55987674ce3d38a3adc126
Thanh Nguyen <dtnguyen.work@gmail.com>
2026-06-22 08:11:58 +0700

    /Users/thanhnguyen/cs336/assignment1-basics/.venv/bin/python

---
+++ cs336_basics/test.py
@@ -0,0 +1,34 @@
+import fast_bpe_bytes as fastBpeBytes
+import fast_bpe_string as fastBpeString
+import logging
+
+# 1. Create a custom logger
+logger = logging.getLogger('fast_bpe_bytes')
+logger.setLevel(logging.WARNING)
+
+# 2. Create handlers
+c_handler = logging.StreamHandler()  # For console
+f_handler = logging.FileHandler('app.log', mode='w')  # For file
+
+# 3. Create formatters and add to handlers
+c_format = logging.Formatter('%(levelname)s - line %(lineno)d - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(lineno)d - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+# 4. Add handlers to the logger
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
+
+## Usage
+if __name__ == '__main__':
+    splitTextToken = "<|endoftext|>"
+    specialTokens = []
+    dataset = "TinyStoriesV2-GPT4-valid.txt"
+    # dataset = "test.txt"
+    # dataset = "corpus.en"
+    fastBpeBytes.run_train_bpe(f"assignment1-basics/data/{dataset}", 
+                output_path=f"assignment1-basics/data/output/{dataset}", 
+                vocab_size=500, special_tokens=specialTokens, split_text_token=splitTextToken, 
+                chunk_size_to_process=100*1024*1024, 
+                get_max_by_cache=True, get_init_multi_process=True, process_count = 4)
